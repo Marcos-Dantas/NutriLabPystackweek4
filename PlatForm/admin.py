375b5528188c866f05bc5ea1ac7abe3e6f20d9c9
Marcos-Dantas <kisnaiver1223@gmail.com>
2022-07-12 11:04:29 -0300

    adicionado favicon

--- PlatForm/admin.py
+++ PlatForm/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from .models import Patients, PatientData, Meal, Option
 
-# Register your models here.
-class PatientsAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Patients._meta.get_fields()]
-
-class PatientDataAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in PatientData._meta.get_fields()]
-
-admin.site.register(Patients,PatientsAdmin)
-admin.site.register(PatientData,PatientDataAdmin)
+admin.site.register(Patients)
+admin.site.register(PatientData)
 admin.site.register(Meal)
 admin.site.register(Option)
